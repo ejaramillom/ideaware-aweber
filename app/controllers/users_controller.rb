659cb990afbc1c023e668c9bea8e8913fb906484
Emmanuel <ejaramilo@hotmail.com>
2021-09-12 13:42:29 -0500

    added new user method and buttons in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: "El usuario fue guardado con éxito"
+    else
+      render :index
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :name, :phone, :sendinformationstatus)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'users#index'
   get '/users', to: 'users#index'
-  get '/users/new', to: 'users#index'
+  get '/users/new', to: 'users#index', as: 'new_user'
+  post '/users', to: 'users#create'
 end
