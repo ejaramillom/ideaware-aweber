ac89f5a54dfc31f46490e9747f7d595089bad0bb
Emmanuel <ejaramilo@hotmail.com>
2021-09-12 14:29:17 -0500

    fully working index api for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,13 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
   def index
     users = User.order( "user_name DESC" )
-
     render json: users
   end
 
@@ -17,5 +23,9 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:email, :name, :phone, :sendinformationstatus)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
