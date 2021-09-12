cf6e10470081a7bb92cd9304c0dd2ed73e0b2fad
Emmanuel <ejaramilo@hotmail.com>
2021-09-12 16:48:45 -0500

    solved issue with user auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ User.create(name: 'Luke', email: 'starwars@universe.com', phone: '16425', sendinformationstatus: True)
